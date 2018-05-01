2ebfa9d2903f80d0344b31b5506f3c67bdb1a658
lisab <lisabowles4797@outlook.com>
2018-05-01 11:37:59 -0400

    I think it works

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require_relative 'hangman.rb'
 
-# $wrong_guess_count = 0
-# $all_guess_letters = []
 
 get '/' do
 	word = ["jacuzzi", "zombies", "awkward", "shallow","species", "buffalo", "cobwebs",
@@ -10,25 +8,25 @@ get '/' do
 	"alfredo", "antique", "bagpipe", "balloon","bandana", "cabbage", "caboose", 
 	"calvary", "dessert", "dolphin", "earmuff", "earring", "emerald", "indulge",
 	"jasmine", "orchids"]
-	@turns = 7     
 	secret_word = word.sample
-	correct_answers = "-" * @secret_word.length
-	erb :home, :locals => {:secret_word => secret_word, :correct_answers => correct_answers}
+	correct_answers = "-" * secret_word.length
+	turns = "7"  
+	erb :home, :locals => {:secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
 end
 
 post '/letter_choice' do
 	letter = params[:letter]
 	secret_word = params[:secret_word]
 	correct_answers = params[:correct_answers]
-	redirect '/gameplay?letter=' + letter + '&secret_word=' + secret_word + '&correct_answers=' + correct_answers
+	turns = params[:turns]
+	redirect '/gameplay?letter=' + letter + '&secret_word=' + secret_word + '&correct_answers=' + correct_answers + '&turns=' + turns
 end
 	
 get '/gameplay' do
 	letter = params[:letter]
 	secret_word = params[:secret_word]
 	correct_answers = params[:correct_answers]
-	secret_word_array = @secret_word.split('')
-	correct_answer_array = @correct_answers.split('')
-	erb :play_game, :locals => {:letter => letter, :secret_word => secret_word}
+	turns = params[:turns]
+	erb :play_game, :locals => {:letter => letter, :secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
 end
 
--- hangman.rb
+++ hangman.rb
@@ -1,48 +1,10 @@
-# puts "Welcome to Hangman."
-
-def secret_word()
-	
-	
-end 
-
-
 
 def game_over?()
 	@turns == 0
 end
 
-def play_game()
-	@guessed_letters = []
-	# puts "you have #{@turns} turns left."
-	# puts "your word is #{@correct_answers}."
-	# print "Please enter a letter:"
-	
-	loop do
-		@secret_word_array.each_with_index do |value, key|
-			if letter == value
-				@correct_answers_array[key] = letter
-			end
-		end	
-		if @secret_word_array.include?(letter)
-			#puts "you guessed a correct letter"
-		else
-			#puts "sorry, that letter is not in the word"
-			@turns -= 1
-			#puts "you have #{@turns} left"
-		end
-
-		@guessed_letters.push(letter)
-		#puts "You have used the following letters: #{@guessed_letters}"
-
-			p @correct_answers_array
-			if game_over?()
-				#puts "Sorry, you have lost the game.  The word was #{@secret_word}"
-				break
-			elsif @correct_answers_array == @secret_word_array
-				#puts "You have won the game"
-				break
-			end
-	end
+def win?()
+	@secret_word_array = @correct_answers_array
 end
 
 
