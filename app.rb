15ea42007aaac3da5c42cc403925c04030556ae2
lisab <lisabowles4797@outlook.com>
2018-05-01 15:15:16 -0400

    It works kinda

--- app.rb
+++ app.rb
@@ -1,32 +1,35 @@
 require 'sinatra'
 require_relative 'hangman.rb'
+enable :sessions
 
 
 get '/' do
-	word = ["jacuzzi", "zombies", "awkward", "shallow","species", "buffalo", "cobwebs",
-	"jackpot","jukebox", "keyhole", "puzzled","zigzags", "zodiacs", "buzzard", 
-	"alfredo", "antique", "bagpipe", "balloon","bandana", "cabbage", "caboose", 
+	word = ["jacuzzi", "zombies", "awkward", "shallow","species", "buffalo", "cobwebs", "jackpot","jukebox", "keyhole", "puzzled","zigzags", "zodiacs", "buzzard", "alfredo", "antique", "bagpipe", "balloon","bandana", "cabbage", "caboose", 
 	"calvary", "dessert", "dolphin", "earmuff", "earring", "emerald", "indulge",
 	"jasmine", "orchids"]
 	secret_word = word.sample
-	correct_answers = "-" * secret_word.length
-	turns = "7"  
-	erb :home, :locals => {:secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
+	correct_word = "-" * secret_word.length
+	secret_word = session[:secret_word]
+	correct_answers = session[:correct_word]
+	turn = "8"
+	turns = session[:turn]  
+	erb :home, :locals => {:correct_word => correct_word, :secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
 end
 
 post '/letter_choice' do
-	letter = params[:letter]
-	secret_word = params[:secret_word]
-	correct_answers = params[:correct_answers]
-	turns = params[:turns]
-	redirect '/gameplay?letter=' + letter + '&secret_word=' + secret_word + '&correct_answers=' + correct_answers + '&turns=' + turns
+	session[:letter] = params[:letter]
+	session[:correct_word] = params[:correct_word]
+ 	session[:secret_word] = params[:secret_word]
+	session[:correct_answers] = params[:correct_answers]
+	session[:turns] = params[:turns]
+	redirect '/gameplay'
 end
 	
 get '/gameplay' do
-	letter = params[:letter]
-	secret_word = params[:secret_word]
-	correct_answers = params[:correct_answers]
-	turns = params[:turns]
-	erb :play_game, :locals => {:letter => letter, :secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
+	letter = session[:letter]
+	correct_word = session[:correct_word]
+	secret_word = session[:secret_word]
+	correct_answers = session[:correct_answers]
+	turns = session[:turns]
+	erb :play_game, :locals => {:correct_word => correct_word, :letter => letter, :secret_word => secret_word, :correct_answers => correct_answers, :turns => turns}
 end
-
--- hangman.rb
+++ hangman.rb
@@ -1,10 +1,10 @@
 
-def game_over?()
-	@turns == 0
+def game_over?(turns)
+	turns == 0
 end
 
-def win?()
-	@secret_word_array = @correct_answers_array
+def win?(secret_word_array, correct_answers_array)
+	secret_word_array = correct_answers_array
 end
 
 
