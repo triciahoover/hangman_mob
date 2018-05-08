d0d534716f011d0f33d5e65e81ffb47476ac08c9
triciahoover <tricia.l.hoover@gmail.com>
2018-05-08 10:11:49 -0400

    almost finished

--- app.rb
+++ app.rb
@@ -5,28 +5,18 @@ require_relative 'hangman.rb'
 get '/' do
 	word = ["jacuzzi", "zombies"]
 	secret_word = word.sample
-	puts secret_word
 	correct_word = "-" * secret_word.length
-	puts correct_word
-	turns = "8"
-	puts turns
+	turns = "10"
 	guessed_letters = " "
-	puts guessed_letters
-	puts guessed_letters.class 
 	erb :home, :locals => {:secret_word => secret_word, :correct_word => correct_word, :turns => turns, :guessed_letters => guessed_letters}
 end
 
 post '/letter_choice' do
 	letter = params[:letter]
-	puts letter
 	secret_word = params[:secret_word]
-	puts secret_word
 	correct_word = params[:correct_word]
-	puts correct_word
- 	turns = params[:turns]
- 	puts turns
-	guessed_letters = params[:guessed_letters]
-	puts guessed_letters
+	turns = params[:turns]
+ 	guessed_letters = params[:guessed_letters]
 	redirect '/gameplay?letter=' + letter + '&secret_word=' + secret_word + '&correct_word=' + correct_word + '&turns=' + turns + '&guessed_letters=' + guessed_letters
 end
 	
--- hangman.rb
+++ hangman.rb
@@ -3,8 +3,8 @@ def game_over?(turns)
 	turns == 0
 end
 
-def win?(secret_word_array, correct_answers_array)
-	secret_word_array == correct_answers_array
+def win?(a,b)
+	a == b
 end
 
 
